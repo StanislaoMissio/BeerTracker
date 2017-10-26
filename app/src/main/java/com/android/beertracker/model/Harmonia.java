c670643611c6e243fd938d538001d33a80f9e988
stanislao <stanis.campregher@gmail.com>
2017-10-26 14:49:08 -0200

    Criação dos models e inserção da lib retrofit no projeto

---
+++ app/src/main/java/com/android/beertracker/model/Harmonia.java
@@ -0,0 +1,44 @@
+package com.android.beertracker.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Harmonia {
+
+    @SerializedName("nome_harmonia")
+    @Expose
+    private String nomeHarmonia;
+
+    @SerializedName("descricao")
+    @Expose
+    private String descricao;
+
+    @SerializedName("cod_harmonia")
+    @Expose
+    private int codHarmonia;
+
+    public String getNomeHarmonia() {
+        return nomeHarmonia;
+    }
+
+    public void setNomeHarmonia(String nomeHarmonia) {
+        this.nomeHarmonia = nomeHarmonia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCodHarmonia() {
+        return codHarmonia;
+    }
+
+    public void setCodHarmonia(int codHarmonia) {
+        this.codHarmonia = codHarmonia;
+    }
+}
