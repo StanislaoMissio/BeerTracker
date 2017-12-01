a528f6a8117340bd7390f60549a1830099f63bac
Stanislao <stanislao.missio@fatec.sp.gov.br>
2017-12-01 18:01:09 -0200

    Arrumar bug de problema para trazer estilos

--- app/src/main/java/com/android/beertracker/activity/Response.java
+++ app/src/main/java/com/android/beertracker/activity/Response.java
@@ -1,29 +1,19 @@
 package com.android.beertracker.activity;
 
 import com.android.beertracker.entity.Estilo;
-import com.android.beertracker.entity.Harmonia;
 
 import java.util.List;
 
 public class Response {
 
-    private List<Harmonia> dataHarmonia;
-    private List<Estilo> dataEstilos;
-
-    public List<Harmonia> getDataHarmonia() {
-        return dataHarmonia;
-    }
-
-    public void setDataHarmonia(List<Harmonia> dataHarmonia) {
-        this.dataHarmonia = dataHarmonia;
-    }
+    private List<Estilo> data;
 
     public List<Estilo> getDataEstilos() {
-        return dataEstilos;
+        return data;
     }
 
     public void setDataEstilos(List<Estilo> dataEstilos) {
-        this.dataEstilos = dataEstilos;
+        this.data = dataEstilos;
     }
 
 
