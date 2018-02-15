5a835bb5d948244400cfb25a390e69c5e00394db
stanislao <stanis.campregher@gmail.com>
2018-02-15 09:07:31 -0200

    Splash Screen

--- app/src/main/java/com/android/beertracker/activity/SplashScreenActivity.java
+++ app/src/main/java/com/android/beertracker/activity/SplashScreenActivity.java
@@ -1,44 +1,42 @@
 package com.android.beertracker.activity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.widget.ImageView;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.android.beertracker.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    ImageView imageLoadingHandLeft;
-    ImageView imageLoadingHandRight;
-    ImageView imageLoadingSparkUp;
-    ImageView imageLoadingSparkDown;
-    private final int DURATION_MILLIS = 300;
-
+    FirebaseAuth auth;
+    FirebaseUser user;
+    Intent intent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
-        imageLoadingHandLeft = findViewById(R.id.loading_animation_hand_left);
+        auth = FirebaseAuth.getInstance();
 
-        imageLoadingHandRight = findViewById(R.id.loading_animation_hand_right);
-
-        imageLoadingSparkUp = findViewById(R.id.loading_animation_spark_up);
-
-        imageLoadingSparkDown = findViewById(R.id.loading_animation_spark_down);
-
-        Animation animation = new AlphaAnimation(1, 0);
-        animation.setDuration(DURATION_MILLIS);
-        animation.setInterpolator(new LinearInterpolator());
-        animation.setRepeatCount(Animation.INFINITE);
-        animation.setRepeatMode(Animation.REVERSE);
-        imageLoadingSparkUp.startAnimation(animation);
-        imageLoadingSparkDown.startAnimation(animation);
+        user = auth.getCurrentUser();
+        if (user != null) {
+            intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
 
     }
 }
