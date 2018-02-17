1e82f92b66c401aee4db7806adf0da580beaab6e
Stanislao <stanislao.missio@fatec.sp.gov.br>
2018-02-17 10:37:03 -0200

    Novo Login com firebase

--- app/src/main/java/com/android/beertracker/activity/LoginActivity.java
+++ app/src/main/java/com/android/beertracker/activity/LoginActivity.java
@@ -2,17 +2,24 @@ package com.android.beertracker.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.beertracker.R;
 import com.android.beertracker.entity.User;
 import com.android.beertracker.infrastructure.OperationListener;
 import com.android.beertracker.manager.UserManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
@@ -22,14 +29,15 @@ public class LoginActivity extends AppCompatActivity {
     EditText passwordText;
     Button buttonLogin;
     TextView signInText;
-    private UserManager userManager;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userManager = new UserManager(this);
+        auth = FirebaseAuth.getInstance();
 
         loginText = findViewById(R.id.login_text);
         passwordText = findViewById(R.id.password_text);
@@ -47,21 +55,18 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                User user = new User(null, passwordText.getText().toString(), loginText.getText().toString());
-                userManager.loginUser(user, new OperationListener() {
-                    @Override
-                    public void onOperationSuccess(Object o) {
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                    @Override
-                    public void onOperationError(Object o, List list) {
-                        Snackbar.make(loginText,"Erro ao efetuar login", Snackbar.LENGTH_SHORT).show();
-                    }
-                });
+                auth.signInWithEmailAndPassword(loginText.getText().toString(), passwordText.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Erro no login", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
